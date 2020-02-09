e676ef52d8dc7410f145df82e435afdfb212fd29
Manisha <manisha04pandey@gmail.com>
2020-02-09 14:46:33 +0530

    Initializing Repository

---
+++ src/Arrays/CountArraySIze.java
@@ -0,0 +1,8 @@
+package Arrays;
+
+public class CountArraySIze {
+    public static void main(String[] args) {
+        int[] A = {1,2,4,5,6,7,78,8};
+        System.out.println("Size = "+A.length);
+    }
+}
