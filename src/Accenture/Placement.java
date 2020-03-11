65b38e46ba860b70d7e46b99daa97bdc6c429464
Manisha <manisha04pandey@gmail.com>
2020-03-11 12:10:50 +0530

    practice

--- src/Accenture/Placement.java
+++ src/Accenture/Placement.java
@@ -1,42 +1,59 @@
 package Accenture;
+import java.util.*;
 
-import java.util.Scanner;
+class Placement{
+         public static void main (String[] args) {
 
-public class Placement {
+             Scanner in=new Scanner(System.in);
+             System.out.println("Enter the no of students placed in CSE:");
+             int c=in.nextInt();
+             System.out.println("Enter the no of students placed in ECE:");
+             int e=in.nextInt();
+             System.out.println("Enter the no of students placed in MECH:");
+            int m=in.nextInt();
 
-    public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int c=in.nextInt();
-        int e=in.nextInt();
-        int m=in.nextInt();
-        System.out.println("Highest placemnet");
-        if(c==e){
-            if(c>e && c>m){
-                System.out.println("CSE");
-                System.out.println("ECE");
-        } else if(c==m){
-                System.out.println("MECH");
-            }
-        }
+             //Negative
+           if(c<0 || e<0 || m<0){
+                     System.out.println("Input is Invalid");
+                 }
+             //equal
+           else if(c==e && e==m){
+                     System.out.println("None of the department has got the highest placement");
+                 }
 
-        if(e>c && e>m){
-            System.out.println("ECE");
-            if(c==e){
-                System.out.println("CSE");
-            }
-            else if(e==m){
-                System.out.println("MECH");
-            }
-        }
+           else{
+                     System.out.println("Highest placement");
+            if(c>e && c>m){                            //if one is highest
+                             System.out.println("CSE");
+                         }
+                   else if(e>c && e>m){
+                             System.out.println("ECE");
+                        }
+                   else if(m>c && m>e){
+                            System.out.println("MECH");
+                         }
+                   else{                                 //2 are same
+                       if(c==e){
+                               if(c>m){
+                                         System.out.println("CSE");
+                                         System.out.println("ECE");
+                    }
+                             }
+                       else if(c==m){
+                               if(c>e){
+                                         System.out.println("CSE");
+                                         System.out.println("MECH");
+                                     }
+                             }
+                       else if(m==e){
+                               if(m>c){
+                                         System.out.println("ECE");
+                                         System.out.println("MECH");
+                                     }
+                             }
+                         }
+                     }
 
-        if(m>e && m>c){
-            System.out.println("MECH");
-            if(m==e){
-                System.out.println("ECE");
-            }
-            else if(c==m){
-                System.out.println("CSE");
-            }
-        }
     }
-}
+    }
+
