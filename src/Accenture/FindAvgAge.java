814fd88c9da28303079a602e02490e5ab75db5f8
Manisha <manisha04pandey@gmail.com>
2020-03-16 01:55:43 +0530

    practice

---
+++ src/Accenture/FindAvgAge.java
@@ -0,0 +1,31 @@
+package Accenture;
+
+import java.util.Scanner;
+
+public class FindAvgAge {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter number odf employees");
+        int n=in.nextInt();
+        System.out.println("Enter ages:");
+        int age[]=new int[n];
+        int sum=0;
+
+        for(int i=0;i<n;i++){
+            age[i]=in.nextInt();
+            if(age[i]<28 || age[i]>40){
+                System.out.println("Enter valid age");
+                break;
+            }else{
+                sum+=age[i];
+
+            }
+
+        }
+        //System.out.println(sum+" "+n);
+
+        double avg=(double) sum/(double)n;
+        String res=String.format("%.2f",avg);
+        System.out.println(res);
+    }
+}
